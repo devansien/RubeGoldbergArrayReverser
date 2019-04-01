88d85d47a157c5b5a38e97524d133a4095a2234a
devansien <devansien@gmail.com>
2019-04-01 22:43:11 +1300

    reverse helper added

---
+++ ReverseHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RubeGoldbergArrayReverser
+{
+    class ReverseHelper
+    {
+        public static object[] Reverse(object[] array)
+        {
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+            for (int i = 0; i < array.Length; i++)
+                dictionary.Add(i.ToString(), array[i]);
+
+            object[] reversed = new object[dictionary.Count];
+            for (int i = 0; i < reversed.Length / 2; i++)
+            {
+                object obj = dictionary[i.ToString()];
+                dictionary[i.ToString()] = dictionary[(reversed.Length - i - 1).ToString()];
+                dictionary[(reversed.Length - i - 1).ToString()] = obj;
+            }
+
+            for (int i = 0; i < reversed.Length; i++)
+                reversed[i] = dictionary[i.ToString()];
+
+            return reversed;
+        }
+    }
+}
